6bd9ea0c6abfa0e9b75652d4fa69b90ae0c709be
Jos van der Zalm <jos.v.d.zalm@gmail.com>
2025-05-09 09:54:41 +0200

    updates vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-    //plugins: [tsconfigPaths()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
